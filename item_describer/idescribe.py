c423d1d6238bd521a58e36578d0c4f77b68832e1
owencking <92190360+owencking@users.noreply.github.com>
2026-04-17 12:15:12 -0400

    allow user to supply custom prompts, better argument handling, better error handling

--- item_describer/idescribe.py
+++ item_describer/idescribe.py
@@ -11,6 +11,7 @@ Writes the description to stdout
 import argparse
 import io
 import os
+import importlib.metadata
 
 import requests
 
@@ -20,13 +21,25 @@ import gbh_ai_helper as ai
 from . import reformat 
 from .prompt_content import pc
 
+# get version number from `pyproject.toml` file
+__version__ = importlib.metadata.version("item-describer")
+
+
+# Global prompt data structure
+# (Can be changed by functions below.)
+PC = pc.copy()
+
+MAX_TOKENS_DEFAULT = 200
+
+############################################################################
 
 def form_system_prompt ( dtype:str="description" ):
     """
     Formulate the system prompt.
     (For now, this returns a fixed string.)
     """
-    system_prompt = pc[dtype]["system_prompt"].strip()
+
+    system_prompt = PC[dtype]["system_prompt"].strip()
 
     return system_prompt
 
@@ -40,20 +53,21 @@ def form_user_prompt( metadata:str,
 
     p = ""
 
-    p += pc[dtype]["user_prompt_instr"].strip()
+    p += PC[dtype]["user_prompt_instr"].strip()
+
     p += "\n\n"
 
-    if pc[dtype]["options"]:
-        p += pc[dtype]["options_intro"].strip()
+    if PC[dtype]["options"]:
+        p += PC[dtype]["options_intro"].strip()
         p += "\n"
-        p += pc[dtype]["options_str"].strip()
+        p += PC[dtype]["options_str"].strip()
         p += "\n\n"
 
-    p += pc[dtype]["metadata_intro"].strip()
+    p += PC[dtype]["metadata_intro"].strip()
     p += "\n```\n"
     p += metadata
     p += "\n```\n\n"
-    p += pc[dtype]["transcript_intro"].strip()
+    p += PC[dtype]["transcript_intro"].strip()
     p += '\n\n"""\n'
     p += transcript
     p += '\n"""\n'
@@ -74,9 +88,9 @@ def get_raw_metadata( aapbid:str ) -> dict:
     fio = io.BytesIO(response.content)
 
     # extract dictionary of important fields and values
-    assttbl, _ = pb.framify.tablify( [fio] )
+    asstdict, _ = pb.framify.dictify( fio )
 
-    return assttbl[0]
+    return asstdict
 
 
 def massage_metadata( asst:dict, dtype="description" ) -> dict:
@@ -133,11 +147,11 @@ def validate_output( raw:str,
 
     if raw.strip().lower() == 'none':
         valid_output = None
-    elif pc[dtype]["options"]:
+    elif PC[dtype]["options"]:
         valid_output = raw.strip()
         out_list = [ t.strip() for t in raw.strip().split('\n') ]
         for t in out_list:
-            if t not in pc[dtype]["options"]:
+            if t not in PC[dtype]["options"]:
                 valid_output = None
     else:
         valid_output = raw.strip()
@@ -149,7 +163,7 @@ def validate_output( raw:str,
 def idescribe( aapbid:str, 
                dtype:str = "description",
                verbose:bool = False,
-               max_tokens:int = 200,
+               max_tokens:int = MAX_TOKENS_DEFAULT,
                deployment_alias = ai.DEFAULT_DEPLOYMENT_ALIAS ) -> str:
     """
     Calls an LLM to generate descriptive metadata for an AAPB item, based
@@ -161,20 +175,31 @@ def idescribe( aapbid:str,
     Second item is the raw LLM output.
     """
 
+    if verbose:
+        print(f"\n* Model deployment alias: {deployment_alias}")
+        print(f"* Model deployment name: {os.getenv(deployment_alias)}")
+        print(f"\n* Ouput metadata type: '{dtype}'")
+        print(f"* Maximum output tokens: {max_tokens}")
+        print()
+
     raw_metadata = get_raw_metadata( aapbid )
+
+    if not raw_metadata:
+        print(f"\nCOULD NOT GET ITEM-LEVEL METADATA FOR AAPB ID: {aapbid}")
+        print("\nWILL NOT ATTEMPT DESCRIPTION.\n")
+        valid_output = raw_output = None
+        return valid_output, raw_output
+
     metadata = massage_metadata( raw_metadata, dtype )
     metadata_str = reformat.format_metadata( metadata )
 
     transcript_url = raw_metadata.get("transcript_url","")
 
     if verbose:
-        print(f"\n* Model deployment alias: {deployment_alias}")
-        print(f"* Model deployment name: {os.getenv(deployment_alias)}")
-        print(f"\n* Ouput metadata type: '{dtype}'")
-        print(f"\n* PROMPT COMPONENTS: ")
+        print(f"* PROMPT COMPONENTS: ")
         for k in ["system_prompt", "user_prompt_instr", "options", "metadata_intro", "transcript_intro"]:
             print(f"\n* {k}:")
-            print(str(pc[dtype][k]).strip())
+            print(str(PC[dtype][k]).strip())
         print(f"\n* AAPB ID: {aapbid}")
         print("\n* Current metadata used:")
         print(metadata_str)
@@ -182,18 +207,27 @@ def idescribe( aapbid:str,
         print(transcript_url)
 
     if not transcript_url.strip():
-        print("\nNO TRANSCRIPT AVAILABLE.  WILL NOT ATTEMPT DESCRIPTION.\n")
+        print("\nNO TRANSCRIPT AVAILABLE.")
+        print("\nWILL NOT ATTEMPT DESCRIPTION.\n")
         valid_output = raw_output = None
+        return valid_output, raw_output
+
     else:
         transcript_text = get_transcript( transcript_url )
-        user_prompt = form_user_prompt( metadata_str, transcript_text, dtype )
+
+        user_prompt = form_user_prompt( metadata_str, 
+                                        transcript_text, 
+                                        dtype )
+        
         system_prompt = form_system_prompt( dtype )
 
         raw_output = ai.one_completion( user_prompt,
                                         system_prompt,
                                         max_tokens=max_tokens,
                                         deployment_alias=deployment_alias )
+        
         valid_output = validate_output( raw_output, dtype )
+
 
     return valid_output, raw_output
 
@@ -201,19 +235,36 @@ def idescribe( aapbid:str,
 
 def main():
 
-    parser = parser = argparse.ArgumentParser(
+    global PC
+
+    app_desc = f"AAPB Item Describer (version {__version__})\n"
+    app_desc += "Generates descriptive metadata for an AAPB item using an LLM.\n\n"
+    app_desc += f"Default LLM deployment alias: {ai.DEFAULT_DEPLOYMENT_ALIAS}\n"
+    app_desc += f"Default LLM deployment name:  {os.getenv(ai.DEFAULT_DEPLOYMENT_ALIAS)}\n"
+
+    class CustomFormatter(argparse.RawDescriptionHelpFormatter, 
+                          argparse.ArgumentDefaultsHelpFormatter):
+        pass
+
+    parser = argparse.ArgumentParser(
         prog='idescribe',
-        description='Generates descirptive metadata for an AAPB item using an LLM.',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        description=app_desc,
+        formatter_class=CustomFormatter
     )
     parser.add_argument("aapbid", metavar="ID",
         help="The AAPB ID for the item you wish to describe.")
-    parser.add_argument("-t", "--type", metavar="TYPE", nargs="?", default="description",
-        help="The type of descriptive metadata you want: 'description' or 'topics'")
-    parser.add_argument("-d", "--deployment", metavar="TYPE", nargs="?", default=ai.DEFAULT_DEPLOYMENT_ALIAS,
-        help="The .gbhai dotfile alias of the model deployment you want to use")        
     parser.add_argument("-v", "--verbose", action="store_true",
         help="Produce verbose/diagnostic output.")
+    parser.add_argument("-t", "--type", metavar="TYPE", nargs="?", default="description",
+        help="The type of descriptive metadata you want: 'description' or 'topics'")
+    parser.add_argument("-s", "--custom-system-prompt", metavar="SYS", nargs="?", default=None,
+        help="The file path to a text file with a custom system prompt")
+    parser.add_argument("-i", "--custom-instruction", metavar="INSTR", nargs="?", default=None,
+        help="The file path to a text file with custom instructions for the prompt")
+    parser.add_argument("-m", "--max-tokens", metavar="MAX", nargs="?", default=MAX_TOKENS_DEFAULT,
+        help="The maximum number of tokens the LLM should produce as its output")
+    parser.add_argument("-d", "--deployment", metavar="DEPLOY", nargs="?", default=ai.DEFAULT_DEPLOYMENT_ALIAS,
+        help="The ~/.gbhai alias of the model deployment you want to use")        
 
     args = parser.parse_args()
 
@@ -233,7 +284,30 @@ def main():
     else:
         dtype = "description"
 
+    if args.custom_system_prompt:
+        if os.path.isfile(args.custom_system_prompt):
+            with open(args.custom_system_prompt) as f:
+                PC[dtype]["system_prompt"] = f.read()
+        else:
+            print("Warning: Invalid path to custom system prompt.  Will use default system prompt.")
 
+    if args.custom_instruction:
+        if os.path.isfile(args.custom_instruction):
+            with open(args.custom_instruction) as f:
+                PC[dtype]["user_prompt_instr"] = f.read()
+        else:
+            print("Warning: Invalid path to custom instructions.  Will use default instructions.")
+
+    if args.max_tokens:
+        try:
+            max_tokens = int(args.max_tokens)
+        except ValueError as e:
+            print("Warning: Invalid value for max tokens.")
+            print(f"  ValueError: {e}")
+            print(f"Will use default value of {MAX_TOKENS_DEFAULT}.")
+            max_tokens = MAX_TOKENS_DEFAULT
+
+        
     deployment_alias = args.deployment
     deployment_name = os.getenv(deployment_alias)
     if not deployment_name:
@@ -243,17 +317,19 @@ def main():
 
     valid_output, raw_output = idescribe( aapbid, 
                                           dtype, 
+                                          max_tokens=max_tokens,
                                           deployment_alias=deployment_alias,
                                           verbose=args.verbose)
 
     if args.verbose:
-        print("\n* OUTPUT:")
-
+        print("\n*** OUTPUT APPEARS BELOW. ***")
+        
+    print()
     if valid_output:
-        print()
         print(valid_output)
     elif raw_output:
-        print("INVALID MODEL OUTPUT:")
+        print("MODEL DID NOT PRODUCE VALID OUTPUT.")
+        print("RAW MODEL OUTPUT:\n")
         print(raw_output)
     else:
         print("NO OUTPUT.")
